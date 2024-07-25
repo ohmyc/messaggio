bf39686f95015386871c7457d41f9e8c3a1086f6
ohmyc <ohmycatile@gmail.com>
2024-07-26 00:09:56 +0300

    Fixed typo

--- internal/echo/config.go
+++ internal/echo/config.go
@@ -4,4 +4,4 @@ import "github.com/ohmyc/messaggio/pkg/config"
 
 var KafkaBootstrapServers = config.Env("KAFKA_BOOTSTRAP_SERVERS")
 var RequestTopic = config.Env("KAFKA_REQUEST_TOPIC")
-var ResponseTopic = config.Env("KAFKA_ESPONSE_TOPIC")
+var ResponseTopic = config.Env("KAFKA_RESPONSE_TOPIC")
